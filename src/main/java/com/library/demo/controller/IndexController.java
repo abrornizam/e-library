bb76e986a849770bc9d3671fb83c30e69f45eeac
Abror Khoerun Nizam <abror.nizam29@gmail.com>
2018-10-15 06:14:12 +0700

    Handling page request not found

--- src/main/java/com/library/demo/controller/IndexController.java
+++ src/main/java/com/library/demo/controller/IndexController.java
@@ -2,10 +2,16 @@ package com.library.demo.controller;
 
 import java.util.List;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.library.demo.model.Buku;
 import com.library.demo.service.BukuService;
@@ -16,7 +22,7 @@ import com.library.demo.service.BukuService;
  */
 
 @Controller
-public class IndexController {
+public class IndexController extends ResponseEntityExceptionHandler {
 
 	@Autowired
 	private BukuService bukuService;
@@ -26,6 +32,20 @@ public class IndexController {
 		List<Buku> listBuku = bukuService.findAll();
 		model.addAttribute("buku", listBuku);
 		return "/perpus/buku/listBuku";
+	}
+		
+	@RequestMapping(value = "/error")
+	public String handleError(HttpServletRequest request) {
+		Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+		if(status != null) {
+			int statusCode = Integer.valueOf(status.toString());
+			if(statusCode == HttpStatus.NOT_FOUND.value()) {
+				return "perpus/error-404";
+			}else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+				return "perpus/error-500";
+			}
+		}
+		return "error";
 	}
 	
 }
---
+++ src/main/java/com/library/demo/controller/PerpusErrorController.java
@@ -0,0 +1,15 @@
+package com.library.demo.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class PerpusErrorController implements ErrorController {
+
+	@Override
+	public String getErrorPath() {
+		// TODO Auto-generated method stub
+		return "/error";
+	}	
+	
+}
